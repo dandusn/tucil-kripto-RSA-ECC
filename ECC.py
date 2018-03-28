79500e90ee6e6a4459aa12bafbfd7ea19d583ff8
HafizhHD <hafizhdary@gmail.com>
2018-03-28 13:34:55 +0700

    ECC
    belum benar

--- ECC.py
+++ ECC.py
@@ -1,6 +1,7 @@
 import math
 import random
 import time
+import os
 
 def isPrime(num):
     for i in range(3, num):
@@ -31,7 +32,7 @@ def gcd(x, y):
 
 
 class EllipCurve(object):
-    # y^2 = x^3 + ax + b
+    # y^2 = x^3 + ax + b mod p
     def __init__(self, p, a, b):
         self.p = p
         self.a = a
@@ -43,7 +44,7 @@ class EllipCurve(object):
         return (res1 == res2)
 
     def findY(self, x):
-        return x * x * x + self.a * x + self.b
+        return math.floor(math.sqrt((x * x * x) + (self.a * x) + (self.b)))
 
 
 class Point(object):
@@ -73,16 +74,16 @@ class Point(object):
         if k == 1:
             return self
         q = Point(0, 1, self.curve)
-        i = 1 << (int(math.log(k, 2)))
+        i = 1 << (k)
         while i > 0:
             q = q + q
-            if k & i == i:
+            if (k & i) == i:
                 q = q + self
             i = i >> 1
         return q
 
     def __str__(self):
-        return '(%s, %s)' % (self.x, self.y)
+        return '(%s,%s)' % (self.x, self.y)
 
 
 def generateKey(curve, pt):
@@ -96,35 +97,96 @@ def generateKey(curve, pt):
 
 
 # di sini m cuma 1 char, kalau mau encrypt banyak perlu iterasi
-def encrypt(publickey, m, curve, pt):
-    k = random.randint(0, curve.p - 1)
+def encrypt(publickey, m, curve, pt, k):
     cx = (publickey * k).x + ord(m)
     cy = curve.findY(cx)
     c1 = Point(cx, cy, curve)
     c2 = pt * k
 #    print(str(c1))
 #    print(str(c2))
-    return (c1, c2)
+    return (c2, c1)
 
 def writefile(fl, w):
-    f = open(fl, "wb")
+    f = open(fl, "w")
     f.write(str(w))
     f.close()
 
+def readfile(fl):
+    with open (fl, "r") as myfile:
+        x = myfile.read()
+    return x
+
+def groupCipher(c):
+    s = c.split()
+    return s
 
 def decrypt(privatekey, c1, c2):
-    return c1.x - (c2 * privatekey).x
+    return c2.x - (c1 * privatekey).x
 
-p = input("input p: ")
-a = input("input a: ")
-b = input("input b: ")
+def encryptString(publickey, s, curve, pt):
+    k = random.randint(0, curve.p - 1)
+    print(k)
+    start = time.time()
+    res = ""
+    for c in s:
+        d = encrypt(publickey, c, curve, pt, k)
+        res += "['" + str(d[0])
+        res += "';'" + str(d[1]) + "']"
+        res += " "
+    res = res[:len(res)-1]
+    done = time.time()
+    
+    print("how long encrypt: %f" % (done-start))
+    print(res)
+    writefile("cipherteks",res)
+    print("file size")
+    print(os.path.getsize("cipherteks"))
+    return res
+
+
+def decryptString(privatekey, c, curve):
+    start = time.time()
+    res = ""
+    for p in c:
+        print(p)
+        tx = p[p.find("[")+1:p.find("]")]
+        t = tx.split(";")
+        t1 = t[0][t[0].find("(")+1:t[0].find(")")]
+        t2 = t[1][t[1].find("(")+1:t[1].find(")")]
+        p1 = t1.split(",")
+        p2 = t2.split(",")
+        po1 = Point(int(p1[0]),int(p1[1]),curve)
+        po2 = Point(int(p2[0]),int(p2[1]),curve)
+        
+        res += chr(decrypt(privatekey, po1, po2))
+        
+    done = time.time()
+    print("how long decrypt: %f" % (done-start))
+    writefile("plainteks",res)
+    print("file size")
+    print(os.path.getsize("plainteks"))
+    return res
+
+p = int(input("input p: "))
+a = int(input("input a: "))
+b = int(input("input b: "))
 crv = EllipCurve(p, a, b)
 
-x = input("x: ")
-print "y : %i" % crv.findY(x)
+x = int(input("x: "))
+print("y: %i" % crv.findY(x))
 
 pt = Point(x, crv.findY(x), crv)
 k = generateKey(crv, pt)
 
-yyy = encrypt(k[1], 'c', crv, pt)
-print(chr(decrypt(k[0], yyy[0], yyy[1])))
+#yyy = encrypt(k[1], 'c', crv, pt)
+#print(chr(decrypt(k[0], yyy[0], yyy[1])))
+
+stu = readfile("plain.txt")
+
+asd = encryptString(k[1], stu, crv, pt)
+
+cip = readfile("cipherteks")
+points = groupCipher(cip)
+
+pqr = decryptString(k[0], points, crv)
+print(pqr)
